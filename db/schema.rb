e8359b96c47b3714f4d0bdceec4a1439fc00d0d6
Lee Hunter <whunter@vt.edu>
2018-06-14 20:41:36 +0000

    remove +s and start on homepage redesign
    add fonts, more styling
    
    more hero styles
    
    moar stylezzzz
    
    remove shadow
    
    COMPEL not Compel
    
    search and fix root_path stuff
    
    more styles
    
    collections section
    
    explore collections list
    
    photo caption
    
    start footer
    
    homepage responsiveness
    
    make search form readable
    
    footer nav
    
    add fonts to git tracking

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180612125729) do
+ActiveRecord::Schema.define(version: 20180612175500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -554,6 +554,8 @@ ActiveRecord::Schema.define(version: 20180612125729) do
     t.binary "zotero_token"
     t.string "zotero_userid"
     t.string "preferred_locale"
+    t.string "provider"
+    t.string "uid"
     t.text "personal_statement"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
