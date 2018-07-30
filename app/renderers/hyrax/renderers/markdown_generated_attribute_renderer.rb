dfda859c8d6f8eadb6fccf167e79d4810672c2f5
Janice Kim <janicekim@vt.edu>
2018-07-30 19:50:43 +0000

    LIBTD-1424: Using Hyrax helper link_to_html (which calls auto_link for sanitizing) when rendering input from tinymce

--- app/renderers/hyrax/renderers/markdown_generated_attribute_renderer.rb
+++ app/renderers/hyrax/renderers/markdown_generated_attribute_renderer.rb
@@ -1,13 +1,12 @@
 module Hyrax
   module Renderers
     class MarkdownGeneratedAttributeRenderer < AttributeRenderer
+      include HyraxHelper 
 
       private
 
         def li_value(value)
-          # Note that html_safe is applied to the value elsewhere in the
-          # inherited AttributeRenderer
-          value
+          link_to_html(value)
         end
     end
   end
