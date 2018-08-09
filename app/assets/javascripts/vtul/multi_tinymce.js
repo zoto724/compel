8a9fd1f12312e89a8ddfbd9481a73afa0bfcb670
compel application owner <hydra@compel.dld.lib.vt.edu>
2018-08-09 20:31:05 +0000

    oops, I forgot about performances

--- app/assets/javascripts/vtul/multi_tinymce.js
+++ app/assets/javascripts/vtul/multi_tinymce.js
@@ -1,15 +1,18 @@
 var tinymceDescriptionNumber = 0;
-var descriptionDiv = 'div.base-terms > div.composition_description';
-var addButton = descriptionDiv + ' button.btn-link.add';
-var editors = descriptionDiv + ' ul.listing > li.field-wrapper.input-group';
+var compositionDiv = 'div.base-terms > div.composition_description';
+var performanceDiv = 'div.base-terms > div.performance_description';
+var addSelector = 'button.btn-link.add';
+var addButton = compositionDiv + ' ' + addSelector + ', ' + performanceDiv + ' ' + addSelector;
+var editorSelector = 'ul.listing > li.field-wrapper.input-group';
+var editors = compositionDiv + ' ' + editorSelector + ', ' + performanceDiv + ' ' + editorSelector;
 
 Blacklight.onLoad(function() {
   $(document).on('click', addButton, function(){
     var $newEditorLi = $(editors).last();
-    var $newTextArea = $newEditorLi.find('textarea.composition_description');
+    var $newTextArea = $newEditorLi.find('textarea.composition_description, textarea.performance_description');
     var $fieldControls = $newEditorLi.find('span.input-group-btn.field-controls');
     $newEditorLi.empty();
-    $newTextArea.attr('id', 'composition_description_' + tinymceDescriptionNumber++);
+    $newTextArea.attr('id', 'description_' + tinymceDescriptionNumber++);
     $newEditorLi.append($newTextArea);
     $newEditorLi.append($fieldControls);
     window.tinyMCE.init({
