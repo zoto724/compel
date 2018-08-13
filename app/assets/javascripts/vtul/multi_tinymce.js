bb4ee5d01907ccdeba6f6c62c8239d487769b83e
compel application owner <hydra@compel.dld.lib.vt.edu>
2018-08-13 18:28:08 +0000

    add support for collections

--- app/assets/javascripts/vtul/multi_tinymce.js
+++ app/assets/javascripts/vtul/multi_tinymce.js
@@ -1,15 +1,16 @@
 var tinymceDescriptionNumber = 0;
 var compositionDiv = 'div.base-terms > div.composition_description';
 var performanceDiv = 'div.base-terms > div.performance_description';
+var collectionDiv = 'div#base-terms > div.collection_description';
 var addSelector = 'button.btn-link.add';
-var addButton = compositionDiv + ' ' + addSelector + ', ' + performanceDiv + ' ' + addSelector;
+var addButton = compositionDiv + ' ' + addSelector + ', ' + performanceDiv + ' ' + addSelector + ', ' + collectionDiv + ' ' + addSelector;
 var editorSelector = 'ul.listing > li.field-wrapper.input-group';
-var editors = compositionDiv + ' ' + editorSelector + ', ' + performanceDiv + ' ' + editorSelector;
+var editors = compositionDiv + ' ' + editorSelector + ', ' + performanceDiv + ' ' + editorSelector + ', ' + collectionDiv + ' ' + editorSelector;
 
 Blacklight.onLoad(function() {
   $(document).on('click', addButton, function(){
     var $newEditorLi = $(editors).last();
-    var $newTextArea = $newEditorLi.find('textarea.composition_description, textarea.performance_description');
+    var $newTextArea = $newEditorLi.find('textarea.collection_description, textarea.composition_description, textarea.performance_description');
     var $fieldControls = $newEditorLi.find('span.input-group-btn.field-controls');
     $newEditorLi.empty();
     $newTextArea.attr('id', 'description_' + tinymceDescriptionNumber++);
